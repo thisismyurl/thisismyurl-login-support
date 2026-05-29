f77d443ac177860b4b448c3ae656e9245372eac0
thisismyurl <122108986+thisismyurl@users.noreply.github.com>
2026-05-29 07:34:48 -0400

    Release 1.6149.0734: bundle hardened GitHub updater as namespaced duplicated code

--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Tags: login, security, wp-login, rate limit, site health, brute force, fail2ban
 Requires at least: 6.0
 Tested up to: 7.0
 Requires PHP: 7.4
-Stable tag: 1.6148.2110
+Stable tag: 1.6149.0734
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
--- thisismyurl-login-support.php
+++ thisismyurl-login-support.php
@@ -3,7 +3,7 @@
  * Plugin Name: This Is My URL - Login Support
  * Plugin URI:  https://thisismyurl.com/
  * Description: Harden login access by allowing a custom login slug and admin security controls.
- * Version:     1.6148.2110
+ * Version:     1.6149.0734
  * Requires at least: 6.4
  * Requires PHP: 7.4
  * Author:      Christopher Ross
@@ -19,7 +19,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'TIMU_LOGIN_SUPPORT_VERSION', '1.6148.2110' );
+define( 'TIMU_LOGIN_SUPPORT_VERSION', '1.6149.0734' );
 
 require_once plugin_dir_path( __FILE__ ) . 'core/class-timu-core.php';
 
